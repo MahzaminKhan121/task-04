d734710826d8dc18cd596036921932e14c43c3e2
MahzaminKhan121 <mdrimonkhan765@gmail.com>
2024-09-17 23:48:52 +0600

    Task 04

---
+++ assets/js/script.js
@@ -0,0 +1,116 @@
+// let a = 10;
+// let b = 20;
+// let c = 30;
+
+// let result = a+c-b;
+// console.log(result);
+
+// let strng = 'Rimon khan';
+// console.log(strng.length);
+// console.log(strng.toUpperCase());
+// console.log(strng.toLowerCase());
+
+
+// // array
+
+// let persons = ['Setu','Sathy','Rison', 'Rimon'];
+
+// console.log(persons);
+// console.log(persons[3]);
+
+
+// // for loop
+// let number = [1,2,3,4,5,6];
+// // console.log(number);
+
+// for (let i=0; i<number.length; i++){
+//     console.log(number[i]);
+// }
+
+/*
+*IF
+*/
+// let num = 5;
+// if (num > 2)
+//     console.log('RMN');
+/*
+*else
+*/
+// let num = 5;
+// if (num == 1) {
+//     console.log('www');
+// } else if (num == 1){
+//     console.log('RAW');
+// }else {
+//     console.log('Enter a valid code.');
+// }
+
+
+/*
+*Switch js
+*/
+
+// let person;
+// // person = 'Tamim';
+// // person = 'Shakib';
+// person = 'Musfiq';
+// switch (person) {
+//     case 'Shakib':
+//         console.log('Number 1 all rounder in the world.');
+//         break;
+//     case 'Tamim':
+//         console.log('Best Batsman in Bangladesh');
+//         break;
+//     case 'Musfiq':
+//         console.log('Mr. Dependable');
+//         break;
+//     default:
+//         console.log('Please enter a valid number');
+// }
+
+
+// let person;
+// // person = 'Rimon';
+// // person = 'Sojib';
+// person = 'Najmul';
+// switch (person){
+//     case 'Rimon':
+//         console.log('Best Bowler in the villege');
+//         break;
+//     case 'Najmul':
+//         console.log('Best batsman');
+//         break;
+//     case 'Sojib':
+//         console.log('Best fuilder');
+//         break;
+//     default:
+//         console.log('Wrong Name type');
+//         break;
+// }
+
+// let person;
+// person = 'Musi';
+// switch (person){
+//     case 'Tamim':
+//         console.log('batsman');
+//         break;
+//     case 'Shakib':
+//         console.log('Alrounder');
+//     case 'Musi':
+//         console.log('wckeeper');
+// }
+
+
+let bangladesh;
+bangladesh = 'Cricket';
+switch (bangladesh){
+    case 'Cricket':
+        console.log('Main game');
+        break;
+    case 'Football':
+        console.log('Second game');
+        break;
+    default:
+        console.log('Enter Valid namee.');
+        break;
+}
